c7f53091021f0bcee49980631a4db5dba9ff4721
lamvd <lamvdavid@gmail.com>
2021-06-24 15:07:08 -0400

    Update to use env vars

--- backend/database.js
+++ backend/database.js
@@ -1,1 +1,12 @@
-export const db = "mongodb://localhost:27017/cryptotracker";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env.local" });
+
+export const db =
+  "mongodb+srv://" +
+  process.env.MONGODB_USER +
+  ":" +
+  process.env.MONGODB_PASS +
+  "@cryptotracker.6xcef.mongodb.net/+" +
+  process.env.MONGODB_DB +
+  "?retryWrites=true&w=majority";
